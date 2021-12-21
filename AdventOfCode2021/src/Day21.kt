43dc705b2e5503fb085edcd2b579fc35bab4b6de
Hiranya Jayathilaka <hiranya911@gmail.com>
2021-12-21 11:49:07 -0800

    fix: Cleaned up day21

--- AdventOfCode2021/src/Day21.kt
+++ AdventOfCode2021/src/Day21.kt
@@ -4,16 +4,22 @@ import java.util.*
 import kotlin.math.min
 
 fun main() {
-    // var game = State(3, 7)
-    var game = State(9, 8)
+    val game = State.withStartingPositions(10, 9)
+    println("Number from deterministic die = ${playWithDeterministicDice(game)}")
+    println("Most quantum wins = ${findMostQuantumWins(game)}")
+}
+
+fun playWithDeterministicDice(init: State): Long {
+    var game = init
     val dice = DeterministicDice()
     while (!game.isDone()) {
         game = game.takeTurn((1..3).sumOf { dice.next() })
     }
 
-    println("Number from deterministic die = ${game.signature()}")
+    return game.signature()
+}
 
-    val init = State(9, 8)
+fun findMostQuantumWins(init: State): Long {
     val frontier = Stack<State>()
     frontier.push(init)
     var p1wins = 0L
@@ -27,48 +33,47 @@ fun main() {
                 p2wins += current.universes
             }
         } else {
-            quantumRolls.map { current.takeTurn(it.key).also { s -> s.universes = current.universes * it.value } }
-                .forEach { frontier.push(it) }
+            quantumRolls.map { current.takeTurn(it.key, it.value) }.forEach { frontier.push(it) }
         }
     }
 
-    println("$p1wins, $p2wins")
+    return if (p1wins > p2wins) p1wins else p2wins
 }
 
 class State(
-    val player1: Int,
-    val player2: Int,
+    private val player1: Int,
+    private val player2: Int,
     val score1: Int = 0,
     val score2: Int = 0,
-    val rolls: Long = 0,
-    val p1Turn: Boolean = true,
+    private val rolls: Long = 0,
+    private val p1Turn: Boolean = true,
+    val universes: Long = 1,
 ) {
-    var universes: Long = 1
 
-    fun takeTurn(sum: Int, r: Int = 3): State =
+    fun takeTurn(sum: Int, unis: Int = 1): State =
         if (p1Turn) {
             val pos = (player1 + sum) % 10
-            State(pos, player2, score1 + pos + 1, score2, rolls + r,false)
+            State(pos, player2, score1 + pos + 1, score2, rolls + 3,false, universes * unis)
         } else {
             val pos = (player2 + sum) % 10
-            State(player1, pos, score1, score2 + pos + 1, rolls + r, true)
+            State(player1, pos, score1, score2 + pos + 1, rolls + 3, true, universes * unis)
         }
 
     fun isDone(winningScore: Int = 1000): Boolean =
         score1 >= winningScore || score2 >= winningScore
 
     fun signature(): Long = min(score1, score2) * rolls
+
+    companion object {
+        fun withStartingPositions(player1: Int, player2: Int): State =
+            State(player1-1, player2-1)
+    }
 }
 
-interface Dice {
-    fun next(): Int
-}
+class DeterministicDice(private val sides: Int = 100) {
+    private var start = 1
 
-class DeterministicDice(val sides: Int = 100): Dice {
-
-    var start = 1
-
-    override fun next(): Int {
+    fun next(): Int {
         val result = start
         start++
         if (start > sides) {
@@ -79,12 +84,4 @@ class DeterministicDice(val sides: Int = 100): Dice {
     }
 }
 
-val quantumRolls = mapOf(
-    3 to 1,
-    4 to 3,
-    5 to 6,
-    6 to 7,
-    7 to 6,
-    8 to 3,
-    9 to 1,
-)
+val quantumRolls = mapOf(3 to 1, 4 to 3, 5 to 6, 6 to 7, 7 to 6, 8 to 3, 9 to 1)
